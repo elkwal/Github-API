7a3f24b1204eedcb23d86fe4687222b769b7246b
Elukwal <elukwal3@gmail.com>
2018-09-24 09:59:04 +0300

    imported the Response to the httpclient

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -1,3 +1,4 @@
 import { Component, OnInit } from '@angular/core';
 import { Github } from '../github-class/github';
 import { HttpClient } from '@angular/common/http';
+import { Response, Http } from '@angular/http';
