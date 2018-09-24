f5c0b31d58c571581285ea4e1699742bee66cb7d
Elukwal <elukwal3@gmail.com>
2018-09-24 10:00:42 +0300

    add the component directive eith selector,templateUrl and styleUrls

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -2,3 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Github } from '../github-class/github';
 import { HttpClient } from '@angular/common/http';
 import { Response, Http } from '@angular/http';
+@Component({
+  selector: 'app-github',
+  templateUrl: './github.component.html',
+  styleUrls: ['./github.component.css']
+})
